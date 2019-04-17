a909c8df37217ceee67234da6c2e98cf3bd9af46
YourFin <d.junkpobox+github@gmail.com>
2019-04-16 22:08:32 -0500

    Add pipfile version checks to docker.sh

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
 FROM python:3.7
 LABEL maintainer="github:YourFin"
 
-  # See: https://github.com/pypa/pipenv/issues/1586#issuecomment-413597222
 RUN pip install --upgrade pip
 RUN pip install pipenv
 
 WORKDIR /app
 
 COPY Pipfile.lock /app/
-  #RUN pipenv lock --clear
+#RUN pipenv lock --clear
 COPY Pipfile /app/
-  # See: https://github.com/pypa/pipenv/issues/2924
-  #RUN pipenv run pip install pip==18.0
 RUN pipenv install --system
 
 ENV PYTHONUNBUFFERED 1
--- docker.sh
+++ docker.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+# Move to script directory
+function finish {
+    popd >/dev/null
+}
+pushd "$(dirname "$0")" >/dev/null || exit 1
+trap finish EXIT
+
+mkdir -p .version_test
+
+# Copy the files over if they don't exist yet
+if [ ! -f '.version_test/Pipfile' ]; then
+    cp -n Pipfile .version_test/Pipfile >/dev/null
+fi
+if [ ! -f '.version_test/Pipfile.lock' ]; then
+    cp -n Pipfile.lock .version_test/Pipfile.lock >/dev/null
+fi
+
+# If the files don't match the cache
+if ! (cmp Pipfile .version_test/Pipfile >/dev/null 2>&1 && cmp Pipfile .version_test/Pipfile >/dev/null 2>&1) ; then
+    docker-compose build queuetime &&
+        cp -f Pipfile .version_test/Pipfile &&
+        cp -f Pipfile.lock .version_test/Pipfile.lock || exit 1
+fi
+
 docker-compose up
