3d4c85833e81d55fc86e03becb31b0f1aa3f2155
YourFin <d.junkpobox+github@gmail.com>
2019-04-21 20:53:21 -0500

    Add wget support to COCO_download.sh

--- COCO_download.sh
+++ COCO_download.sh
@@ -7,17 +7,24 @@ function finish {
 pushd "$(dirname "$0")" >/dev/null || exit 1
 trap finish EXIT
 
-type curl &>/dev/null || (echo "curl not installed, please install it to continue or use docker" && exit 1)
-type unzip &>/dev/null || (echo "unzip not installed, please install it to continue or use docker" && exit 1)
+which curl &>/dev/null || which wget &>/dev/null || (echo "curl not installed, please install it to continue or use docker" && exit 1)
+which unzip &>/dev/null || (echo "unzip not installed, please install it to continue or use docker" && exit 1)
 
-scratch=$(mktemp -d -t tmp.XXXXXXXXXX) || (echo "Error: could not creat temp directory" && exit 1)
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX) || (echo "Error: could not create temp directory" && exit 1)
 function clean_scratch {
     rm -r $scratch
     finish
 }
 
-echo 'Downloading annotiations...'
-curl http://images.cocodataset.org/annotations/annotations_trainval2017.zip > "$scratch/annotations.zip"
+echo 'Downloading annotations...'
+if which curl &>/dev/null ; then
+    curl http://images.cocodataset.org/annotations/annotations_trainval2017.zip > "$scratch/annotations.zip"
+elif which wget &>/dev/null ; then
+    wget --no-config http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O "$scratch/annotations.zip"
+else
+    echo "curl AND wget not installed, please install one to continue or use docker"
+    exit 1
+fi
 unzip "$scratch/annotations.zip" -d ./data/coco/
 
 python ./src/download.py
