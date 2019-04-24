83a37fa1d934d1071b36665a0bdaa7f3db1ef374
YourFin <d.junkpobox+github@gmail.com>
2019-04-24 14:08:06 -0500

    preprocessing.py: first draft of gen_training_tensors, image_gen_factory

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 from math import ceil
 import file_management
@@ -55,20 +56,36 @@ def pad_image(mask):
 #  To generate the ground truth corresponding to a given image.
 # Parameters:
 #  coco: COCO - a coco instance to pull annotation information from
-#  bounding_box_count: int - number of bounding boxes per cell; known as B in YOLO paper.
+#  bounding_box_count: int - number of bounding boxes per cell; known as B in YOLO paper
+#    NOTE: Dead paramater for now - only works with one
 #  cell_width: int - the width in pixels of a cell in the image.
 #  cell_height: int - the height in pixels of a cell in the image.
-#  img: pycocotools.img - image metadata object for the image
+#  img_id: id - id for the image
 # Produces:
 #  output: tensor[double] - A tensor of training data
 # Preconditions:
 #  coco is initialized with valid data
 #  cell_width < width of image
 #  cell_height < height of image
-def gen_training_tensors(coco, bounding_box_count, cell_width, cell_height, img):
-    # These should probably get moved to the parameters - taken directly from YOLO paper
+#  bounding_box_count >= 1
+def gen_training_tensors(coco, bounding_box_count, cell_width, cell_height, img_id):
+    # Force bounding_box_count to 1 - See NOTE in above documentation
+    bounding_box_count = 1
+
+    # These should probably get moved to the parameters
     DEFAULT_LOCATION = 0
-    DEFAULT_WEIGHT = 0
+    NO_OBJECT_WEIGHT = 0
+    HAS_OBJECT_WEIGHT = 1
+
+    # Position of the various training parameters along the last dimension
+    # of the output data from the neural network
+    POS_BOX_CENTER_X = 0
+    POS_BOX_CENTER_Y = 1
+    POS_BOX_WIDTH = 2
+    POS_BOX_HEIGHT = 3
+    POS_OBJ_LIKELYHOOD = 4
+
+    img = coco.loadImgs([img_id])
     person_cat_ids = coco.getCatIds(catNms=['person'])
     annotation_ids = coco.getAnnIds(imgIds=[img['id']], catIds=person_cat_ids)
     annotations = coco.loadAnns(annotation_ids)
@@ -77,13 +94,75 @@ def gen_training_tensors(coco, bounding_box_count, cell_width, cell_height, img)
     cell_y_count = ceil(img.shape[1] / cell_height)
     # 5 parameters to each bounding box: Probability, X pos, Y pos, Width, Height
     training_data = np.full((cell_x_count, cell_y_count, bounding_box_count * 5), DEFAULT_LOCATION)
-    # Set all object probabilities to lambda_noobj
-    if DEFAULT_LOCATION != DEFAULT_WEIGHT:
-        training_data[..., ..., 4: :5] = DEFAULT_WEIGHT
+    # Set all object probabilities to NO_OBJECT_WEIGHT
+    if DEFAULT_LOCATION != NO_OBJECT_WEIGHT:
+        training_data[..., ..., 4: :5] = NO_OBJECT_WEIGHT
 
     for annotation in annotations:
-        annotation
+        # Calculate the cell that the annotation should match
+        bounding_box = annotation['bbox']
+        corner_x = int(bounding_box[0])
+        corner_y = int(bounding_box[1])
+        width = int(bounding_box[2])
+        height = int(bounding_box[3])
 
+        # Find the center of the box in terms of the whole image
+        # These values are purposely floats to keep as much information as
+        #  possible about the center of the image
+        center_x = corner_x + width / 2
+        center_y = corner_y + height / 2
+
+        # Calculate the cell the bounding box is centered in
+        cell_x_pos = floor(center_x / cell_width)
+        cell_y_pos = floor(center_y / cell_height)
+
+        # Find the center of the box relative to the corner of the cell:
+        center_rel_x = center_x - (cell_x_pos * cell_width)
+        center_rel_y = center_y - (cell_y_pos * cell_height)
+
+        # ...And put it in terms of the cell size
+        center_rel_x = center_rel_x / cell_width
+        center_rel_y = center_rel_y / cell_height
+
+        # Find the size of the bounding box relative to the cell
+        rel_width = width / cell_width
+        rel_height = height / cell_height
+
+        # TODO: Move to handling more than one bounding box
+        if training_data[cell_x_pos, cell_y_pos, 4] != NO_OBJECT_WEIGHT:
+            logging.warn("Image %d has multiple bounding boxes in cell (%d,%d)" % (
+                img_id,
+                cell_x_pos,
+                cell_y_pos
+            ))
+
+        # Set values for the training data
+        training_data[cell_x_pos, cell_y_pos, POS_BOX_CENTER_X] = center_rel_x
+        training_data[cell_x_pos, cell_y_pos, POS_BOX_CENTER_Y] = center_rel_y
+        training_data[cell_x_pos, cell_y_pos, POS_BOX_WIDTH] = rel_width
+        training_data[cell_x_pos, cell_y_pos, POS_BOX_HEIGHT] = rel_height
+        training_data[cell_x_pos, cell_y_pos, POS_OBJ_LIKELYHOOD] = HAS_OBJECT_WEIGHT
+
+    return training_data
+
+
+
+# Procedure:
+#  image_gen_factory
+# Purpose:
+#  To create a generator that yields numpy arrays corresponding to image ids
+# Parameters:
+#  image_ids: [int] - list of image ids to return
+#  buffer_size: int = 0 - buffer images ahead of time
+# Produces:
+#  output: generator(np.array(float32)) - generator of numpy image arrays
+# Preconditions:
+#  image_ids refer to actual images on disk as defined in file_management.py
+# Postconditions:
+#  image_id is read in with the thing
+def image_gen_factory(image_ids, buffer_size=0):
+    for id in image_ids:
+        yield file_management.get_image(id)
 
 # Returns a generator of tuples: (img, training tensor)
 # Normalize the image matrix (set all the value in the range
