2996bbf4de283daf58f41f1cc2a73977d5d926ca
Xinya Yang <yangxiny@grinnell.edu>
2019-04-30 23:37:47 -0500

    removed the metrics in compile

--- src/train.py
+++ src/train.py
@@ -111,8 +111,7 @@ print("[INFO] compiling model...")
 model = build(width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
 	depth=IMAGE_DIMS[2], classes=NUM_CLASSES)
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss=QueueTime_loss, optimizer=opt,
-	metrics=["accuracy"]) # not sure about the metrics, decided later
+model.compile(loss=QueueTime_loss, optimizer=opt) # not sure about the metrics, decided later
 
 # train the network
 print("[INFO] training network...")
