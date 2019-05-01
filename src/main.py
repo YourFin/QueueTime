077410eb55d2a63c51050b98432fdee5cf55ef1a
YourFin <d.junkpobox+github@gmail.com>
2019-04-30 20:13:19 -0500

    Add new entrypoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -i
 
-from keras.layers import Dense, Dropout, Activation, Flatten
+from pycocotools.coco import COCO
+from file_management import ANNOTATION_FILE, get_downloaded_ids
+from annotations import get_image_annotations, plot_annotations
 
-from keras.layers import Convolution2D, MaxPooling2D
-
-import cocoapi.pycocotools
+coco = COCO(ANNOTATION_FILE)
+imgs_ids = get_downloaded_ids()
+anns = [get_image_annotations(coco, img) for img in imgs]
