42ff79c132724a796075fe5e599ec9f8a99a70c1
Xinya Yang <yangxiny@grinnell.edu>
2019-05-01 04:45:47 -0500

    added print statement

--- src/QueueTimeNet.py
+++ src/QueueTimeNet.py
@@ -171,15 +171,15 @@ def QueueTime_loss(y_true, y_pred): # should be a CELL_ROW * CELL_COL * 5 tensor
 
 	indicator = y_true[...,0]
 	print("[INFO] indicator", indicator)
-	x_loss = K.square(y_true[...,1] - y_pred[...,1])
+	x_loss = K.square(y_true[...,1] - y_pred[...,1]) #3d
 	print("[INFO] x loss", x_loss)
 	y_loss = K.square(y_true[...,2] - y_pred[...,2])
 	print("[INFO] y loss", y_loss)
-	xy_loss += coord * K.sum(indicator*(y_loss+x_loss))
+	xy_loss += coord * K.sum(K.sum(K.sum(indicator*(y_loss+x_loss),0), 0), 0)
 	print("[INFO] xy_loss", xy_loss)
-	K.print_tensor(xy_loss)
-	a = xy_loss[0]
-	print("[[[[[INFO]]]]] a", a)
+
+
+
 	w_loss = K.square(K.sqrt(y_true[...,3]) - K.sqrt(y_pred[...,3]))
 	h_loss = K.square(K.sqrt(y_true[...,4]) - K.sqrt(y_pred[...,4]))
 	wh_loss += coord * np.sum(indicator*(w_loss+h_loss),None)
