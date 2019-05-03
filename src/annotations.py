be85bb3c89815e8efc68cd7be440591de50bc7b8
YourFin <d.junkpobox+github@gmail.com>
2019-05-03 13:54:20 -0500

    annotations.py: annotations from output working

--- src/annotations.py
+++ src/annotations.py
@@ -1,6 +1,7 @@
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 from file_management import get_image
+from math import floor
 
 # Procedure:
 #  plot_annotations
@@ -52,3 +53,67 @@ def get_image_annotations(coco, img_id):
     person_cat_ids = coco.getCatIds(catNms=['person'])
     annotation_ids = coco.getAnnIds(imgIds=[img['id']], catIds=person_cat_ids, iscrowd=False)
     return coco.loadAnns(annotation_ids)
+
+# Procedure:
+#  annotations_from_output
+# Purpose:
+#  To generate absolute coordinate annotation data from the neural network output
+# Paramaters:
+#  cell_width: int - width of cells in pixels
+#  cell_height: int - height of cells in pixels
+#  output_data: numpy[NxMxB*5] - output data from neural network
+#  threshold: float=
+#   (same format as ground truth data)
+# Produces:
+#  bounding_boxes: [{'bbox': (int, int, int, int), 'likelyhood': int}] - list of bounding boxes as in coco dataset
+# Preconditions:
+#  no additional
+# Postconditions:
+#  each bbox entry will be of the form (x_pos, y_pos, width, height)
+#  Algorithm:
+#   Transform values back to pixels:
+#    width *= cell_width
+#    height *= cell_height
+#   Transform coordinates back to coco style list as (x_pos, y_pos,width, height)
+#   likelyhood key contains calculated likelyhood of the bounding box
+def annotations_from_output(cell_width, cell_height, output_data):
+    # Need to lift into upper file
+    POS_BOX_CENTER_X = 1
+    POS_BOX_CENTER_Y = 2
+    POS_BOX_WIDTH = 3
+    POS_BOX_HEIGHT = 4
+    POS_OBJ_LIKELYHOOD = 0
+    (x_cells, y_cells, channels) = output_data.shape
+
+    bounding_boxes = []
+
+    for x_cell in range(x_cells):
+        for y_cell in range(y_cells):
+            for box_num in range(floor(channels / 5)):
+                rel_width = output_data[x_cell, y_cell, box_num * 5 + POS_BOX_WIDTH]
+                rel_height = output_data[x_cell, y_cell, box_num * 5 + POS_BOX_HEIGHT]
+
+                absolute_width = rel_width * cell_width
+                absolute_height = rel_height * cell_height
+
+                rel_box_center_x = output_data[x_cell, y_cell, box_num * 5 + POS_BOX_CENTER_X]
+                rel_box_center_y = output_data[x_cell, y_cell, box_num * 5 + POS_BOX_CENTER_Y]
+
+                cell_box_center_x = rel_box_center_x * cell_width
+                cell_box_center_y = rel_box_center_y * cell_height
+
+                cell_box_ul_x = cell_box_center_x - absolute_width / 2
+                cell_box_ul_y = cell_box_center_y - absolute_height / 2
+
+                box_ul_x = cell_box_ul_x + (x_cell * cell_width)
+                box_ul_y = cell_box_ul_y + (y_cell * cell_height)
+
+                person_likelyhood = output_data[x_cell, y_cell, box_num * 5 + POS_OBJ_LIKELYHOOD]
+
+                bounding_box = {
+                    'bbox': [box_ul_x, box_ul_y, absolute_width, absolute_height],
+                    'likelyhood': person_likelyhood
+                }
+                bounding_boxes.append(bounding_box)
+
+    return bounding_boxes
