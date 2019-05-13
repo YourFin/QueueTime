089847147a22ef5b986189027ebfe64bd4d88268
YourFin <d.junkpobox+github@gmail.com>
2019-05-12 22:22:22 -0500

    pretrained-labeling: attempt at classification

--- pretrained-labeling/gen_labels.py
+++ pretrained-labeling/gen_labels.py
@@ -13,9 +13,18 @@ DATASET_DIR = '%s/data/coco' % QUEUETIME_DIR
 ANNOTATION_FILE = '%s/annotations/instances_train2017.json' % DATASET_DIR
 
 ROOT_DIR = dirname(os.path.abspath(__file__))
+MODEL_DIR = ROOT_DIR + '/model_data'
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+PERSON_CATEGORY_ID = 1
 
 # Borrowed heavily from: https://medium.com/@ageitgey/snagging-parking-spaces-with-mask-r-cnn-and-python-955f2231c400``
+
+def assert_model_downloaded():
+    "Downloads the pre-trained coco model if need be"
+    if not os.path.exists(COCO_MODEL_PATH):
+        mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
+        assert os.path.exists(COCO_MODEL_PATH), "Download failed"
+
 
 # Configuration that will be used by the Mask-RCNN library
 class MaskRCNNConfig(mrcnn.config.Config):
@@ -25,18 +34,31 @@ class MaskRCNNConfig(mrcnn.config.Config):
     NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
     DETECTION_MIN_CONFIDENCE = 0.6
 
-def assert_dataset_downloaded():
-    "Downloads the pre-trained coco model if need be"
-    if not os.path.exists(COCO_MODEL_PATH):
-        mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
-
 
 def evaluate_video(video_path):
     assert os.path.exists(video_path), "Video does not exist"
     vidstream = cv2.VideoCapture(video_path)
 
+    model = MaskRCNN(mode='inference', model_dir=MODEL_DIR, config=MaskRCNNConfig())
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
 
+    frame_num = -1
+    # TODO: remove frame number cap
+    while vidstream.isOpened() and frame_num < 10:
+        frame_num += 1
 
+        success, frame = vidstream.read()
+        if not success:
+            # Stream is empty
+            break
+
+        # Convert from brg to rgb
+        frame = frame[:,:,::-1]
+
+        # Run through the model, grabbing the only frame
+        result = model.detect([frame], verbose=0)
+
+        print(result)
 
 if __name__ == '__main__':
     import argparse
@@ -45,4 +67,7 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video-path", type=Path, help="Path to input video")
     ap.add_argument("-o", "--output", type=Path, help="Path to output file")
-    args = ap.parse_args()
+    arguments = vars(ap.parse_args())
+
+    assert_model_downloaded()
+    evaluate_video(str(arguments["video_path"]))
