45fdfc6ea64d7df7401134c3b14efd9e74aa1880
Xinya Yang <yangxiny@grinnell.edu>
2019-05-12 23:39:49 -0500

    reset learning rate

--- src/QueueTimeNet.py
+++ src/QueueTimeNet.py
@@ -165,7 +165,7 @@ def QueueTime_loss(y_true, y_pred): # should be a BS * CELL_ROW * CELL_COL * 5 t
 	print("[INFO] ypred", y_pred)
 
 	coord = 3
-	noobj = 0.01
+	noobj = 0.07
 
 	indicator = y_true[...,0]
 	print("[INFO] indicator", indicator)
